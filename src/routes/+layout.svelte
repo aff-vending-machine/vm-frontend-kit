8d52566061e4f4048856234362dc787560a6c488
Tanawat Hongthai <ztrixack.th@gmail.com>
2023-10-31 01:00:27 +0700

    chore: update project structure and tailwindcss

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import '../app.css';
+</script>
+
+<svelte:head>
+  <title>Vending Machine</title>
+  <meta name="description" content="The web application for vending machine" />
+  <meta name="theme-color" content="#000000" />
+</svelte:head>
+
+<slot />
